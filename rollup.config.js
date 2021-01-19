f080eaf1d4699519a225c8fc1f0f9c0244c301a4
汪東陽 <michael19920327@gmail.com>
2021-01-20 01:47:56 +0800

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { DEFAULT_EXTENSIONS as BABEL_DEFAULT_EXTENSIONS } from '@babel/core';
+import { nodeResolve, DEFAULTS as NODE_RESOLVE_DEFAULTS } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
@@ -10,23 +11,50 @@ import {
   name,
 } from './package.json';
 
-const extensions = ['.js', '.ts'];
+const TS_EXTENSIONS = ['.ts', '.tsx'];
 
-export default {
+/**
+ * @param {string} str
+ */
+const camalize = (str) => str
+  .toLowerCase()
+  .replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => chr.toUpperCase());
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
   input: 'src/index.ts',
   output: [
-    { file: main, format: 'cjs' },
-    { file: module, format: 'es' },
-    { name, file: browser, format: 'umd' },
+    {
+      file: main,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+    },
+    {
+      file: module,
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      name: camalize(name),
+      file: browser,
+      format: 'umd',
+      sourcemap: true,
+      exports: 'named',
+    },
   ],
   plugins: [
-    nodeResolve({ extensions }),
+    nodeResolve({ extensions: [...NODE_RESOLVE_DEFAULTS.extensions, ...TS_EXTENSIONS] }),
     commonjs(),
     babel({
-      extensions,
-      presets: ['@babel/preset-env', '@babel/typescript'],
+      extensions: [...BABEL_DEFAULT_EXTENSIONS, ...TS_EXTENSIONS],
+      presets: ['@babel/env', '@babel/typescript'],
       babelHelpers: 'bundled',
     }),
     terser(),
   ],
 };
+
+export default config;
